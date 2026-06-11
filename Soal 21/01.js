86e39b1faa2e78923e39ee10298bd250bd6fdaf9
M. FARIZ Z.A <mhmdfarizza@gmail.com>
2026-06-11 17:54:50 +0700

    pengerjaan soal 21

---
+++ Soal 21/01.js
@@ -0,0 +1,3 @@
+console.log('********************************')
+console.log('    UJIAN SOAL 21 M FARIZ Z A   ')
+console.log('--------------------------------')
--- Soal 21/1.js
+++
@@ -1,3 +0,0 @@
-console.log('**********************************')
-console.log('    UJIAN ALGORITMA M FARIZ Z A   ')
-console.log('----------------------------------')
